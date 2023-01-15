612e015bb2bb408ccdcc38a7b36f8f415964e375
Syusina Daria <syusinad@mail.ru>
2023-01-15 18:46:07 +0500

    add change

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,4 +4,4 @@ import getGreeting from '../src/cli.js';
 
 const nameGame = 'brain-games';
 const gameRules = 'What is the result of the expression?';
-const gretting = getGreeting(nameGame, gameRules);
+getGreeting(nameGame);
