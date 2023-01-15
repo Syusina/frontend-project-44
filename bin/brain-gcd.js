7b33c161c74eeeecd783405ea574d49a035b1cc4
Syusina Daria <syusinad@mail.ru>
2023-01-15 17:38:35 +0500

    update games

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,28 +1,9 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { getGreeting, getTested, startGame } from '../src/index.js';
-import generateRandomNumber from '../src/generator-random.js';
+import { getGreeting, startGame } from '../src/index.js';
+import startRound from '../src/games/for-brain-gcd.js';
 
 const nameGame = 'brain-gcd';
 const gameRules = 'Find the greatest common divisor of given numbers.';
 const gretting = getGreeting(nameGame, gameRules);
-
-const startRound = () => {
-  let firstNumber = generateRandomNumber();
-  let secondNumber = generateRandomNumber();
-  const userAnswer = readlineSync.question(`Question: ${firstNumber} ${secondNumber} `).toString();
-  console.log(`Your answer: ${userAnswer} `);
-
-  while (firstNumber !== secondNumber) {
-    if (firstNumber > secondNumber) {
-      firstNumber -= secondNumber;
-    } else {
-      secondNumber -= firstNumber;
-    }
-  }
-  const correctAnswer = firstNumber.toString();
-  const result = getTested(userAnswer, correctAnswer, gretting);
-  return result;
-};
 
 startGame(startRound, gretting);
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,20 +1,9 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { getGreeting, getTested, startGame } from '../src/index.js';
-import generateRandomNumber from '../src/generator-random.js';
-import isPrime from '../src/isPrime.js';
+import { getGreeting, startGame } from '../src/index.js';
+import startRound from '../src/games/for-brain-prime.js';
 
 const nameGame = 'brain-prime';
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const gretting = getGreeting(nameGame, gameRules);
-
-const startRound = () => {
-  const numberForQuestion = generateRandomNumber();
-  const userAnswer = readlineSync.question(`Question: ${numberForQuestion} `).toLowerCase();
-  console.log(`Your answer: ${userAnswer} `);
-  const correctAnswer = (isPrime(numberForQuestion)) ? 'yes' : 'no';
-  const result = getTested(userAnswer, correctAnswer, gretting);
-  return result;
-};
 
 startGame(startRound, gretting);
