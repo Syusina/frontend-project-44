a7e26f5fd383f4b80bb40665b95ce269657a2c39
Syusina Daria <syusinad@mail.ru>
2023-01-16 07:19:08 +0500

    fixed brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import { getGreeting, startGame } from '../src/index.js';
-import startRound, { nameGame, gameRules } from '../src/games/even.js';
+import letsPlay from '../src/games/even.js';
 
-const gretting = getGreeting(nameGame, gameRules);
-startGame(startRound, gretting);
+letsPlay();
--- src/games/even.js
+++ src/games/even.js
@@ -1,18 +1,24 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import { getTested } from '../index.js';
+import { getGreeting, getTested, startGame } from '../index.js';
 import generateRandomNumber from '../generator-random.js';
 
-export const nameGame = 'brain-even\n';
-export const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const letsPlay = () => {
+  
+  const nameGame = 'brain-even\n';
+  const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const gretting = getGreeting(nameGame, gameRules);
 
-const startRound = (gretting) => {
-  const numberForQuestion = generateRandomNumber();
-  console.log(`Question: ${numberForQuestion}`);
-  const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-  const correctAnswer = (numberForQuestion % 2 === 0) ? 'yes' : 'no';
-  const result = getTested(userAnswer, correctAnswer, gretting);
-  return result;
+  const startRound = (gretting) => {
+    const numberForQuestion = generateRandomNumber();
+    console.log(`Question: ${numberForQuestion}`);
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    const correctAnswer = (numberForQuestion % 2 === 0) ? 'yes' : 'no';
+    const result = getTested(userAnswer, correctAnswer, gretting);
+    return result;
+  };
+
+startGame(startRound, gretting);
 };
 
-export default startRound;
+export default letsPlay;
