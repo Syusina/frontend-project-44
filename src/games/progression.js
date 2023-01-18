15c764971653edd16508bb761b09b9dac6b3cfb2
Syusina Daria <syusinad@mail.ru>
2023-01-18 14:47:42 +0500

    fixed progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,16 +5,16 @@ import generateRandomNumber from '../generator-random.js';
 const gameRules = 'What number is missing in the progression?';
 const startRound = () => {
   const allNumbers = [];
-  let startNumber = generateRandomNumber(1, 100);
-  const multiplier = generateRandomNumber(1, 100);
-  const quantity = 10;
+  let startNumber = generateRandomNumber(1, 10);
+  const multiplier = generateRandomNumber(1, 10);
+  const quantity = generateRandomNumber(6, 11);
   let i = 0;
   while (i < quantity) {
     allNumbers.push(startNumber);
     startNumber += multiplier;
     i += 1;
   }
-  const passNumber = generateRandomNumber(0, 10);
+  const passNumber = generateRandomNumber(0, quantity);
   const numbersStr = `${allNumbers.slice(0, passNumber)} .. ${allNumbers.slice(passNumber + 1, allNumbers.length - 1)}`;
   const numbersArr = numbersStr.split(',');
   const numberForQuestion = numbersArr.join(' ');
