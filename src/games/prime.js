722578a4b69b5c3e90a3770dfee455d7de53a6d8
Syusina Daria <syusinad@mail.ru>
2023-01-18 13:11:10 +0500

    fixed

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 import startGame from '../index.js';
 import generateRandomNumber from '../generator-random.js';
-import isPrime from '../isPrime.js';
 
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const startRound = () => {
   const numberForQuestion = generateRandomNumber();
+  const isPrime = (num) => {
+    if (num <= 1) {
+      return false;
+    } else if (num === 2) {
+        for (let i = 2; i < num; i += 1) {
+          if (num % i === 0) {
+            return false;
+          }
+        }
+        return true;
+    }
+  };
   const correctAnswer = (isPrime(numberForQuestion)) ? 'yes' : 'no';
   return [numberForQuestion, correctAnswer];
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 
 const startGame = (gameRules, startRound) => {
 // приветствие
-  console.log(`Welcome to the Brain Games!`);
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n${gameRules}`);
 
