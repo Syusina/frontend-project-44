0f2d5732b7f23b1a5a77d8759565804ed9cb1803
Syusina Daria <syusinad@mail.ru>
2023-01-23 18:11:08 +0500

    add edits after review

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,7 +2,6 @@
 import startGame from '../index.js';
 import generateRandomNumber from '../generator-random.js';
 import getProgression from '../getProgression.js';
-import getPassNumber from '../passNumber.js';
 
 const description = 'What number is missing in the progression?';
 const getQuestionAndAnswer = () => {
@@ -11,8 +10,11 @@ const getQuestionAndAnswer = () => {
   const length = generateRandomNumber(5, 10);
   const progression = getProgression(startNumber, step, length);
   const passIndex = generateRandomNumber(0, length);
-  const questionAndAnswer = getPassNumber(progression, passIndex);
-  return questionAndAnswer;
+  const passNumber = progression[passIndex];
+  progression[passIndex] = '..';
+  const numberForQuestion = progression.join(' ');
+  const correctAnswer = passNumber.toString();
+  return [numberForQuestion, correctAnswer];
 };
 
 const letsPlay = () => startGame(description, getQuestionAndAnswer);
